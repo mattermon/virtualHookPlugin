02a3103e4244cb31dccb4dc9399d697b002793db
mattermon <mattermon@163.com>
2017-08-10 19:11:00 +0800

    初次提交\r\n代码直接可用

---
+++ README.md
@@ -0,0 +1,43 @@
+ChangePhoneInfo
+----------------
+
+- forked from
+    [ChangePhoneInfo](https://github.com/rk700/ChangePhoneInfo).
+
+# Introduction
+This is a hook plugin used by [VirtualHook](https://github.com/rk700/VirtualHook). It hooks some Java APIs that are used for getting device info, and all the values are hard-codeded as a demo.
+
+Currently the following methods are hooked:
+
+- android.bluetooth.BluetoothAdapter.getAddress()
+- android.telephony.gsm.GsmCellLocation.getCid()
+- android.telephony.gsm.GsmCellLocation.getLac()
+- android.location.Location.getLatitude()
+- android.location.Location.getLongitude()
+- android.telephony.TelephonyManager.getDeviceId()
+- android.telephony.TelephonyManager.getLine1Number()
+- android.telephony.TelephonyManager.getSimOperator()
+- android.telephony.TelephonyManager.getSimSerialNumber()
+- android.telephony.TelephonyManager.getSubscriberId()
+- android.net.wifi.WifiInfo.getBSSID()
+- android.net.wifi.WifiInfo.getSSID()
+- android.net.wifi.WifiInfo.getIpAddress()
+- android.net.wifi.WifiInfo.getMacAddress()
+
+And these system property values are changed as well by hooking native function `__system_property_get()`:
+
+- ro.serialno
+- ro.product.brand
+- ro.product.device
+- ro.build.tags
+- ro.product.model
+- ro.product.manufacturer
+
+Finally, `Build.SERIAL` is changed by simple Java reflection.
+
+#增加的hook函数
+- StringBuilder.toString()        包括打印toString的结果
+
+# Usage
+Build and push the output apk to `/sdcard/`. Then add and start applications in VirtualHook.
+
---
+++ app/src/main/java/lab/galaxy/yahfa/HookInfo.java
@@ -0,0 +1,44 @@
+package lab.galaxy.yahfa;
+
+import android.os.Build;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by liuruikai756 on 17/05/2017.
+ */
+
+public class HookInfo {
+    static {
+        System.loadLibrary("hookprop");
+
+        try {
+            // change Build.SERIAL
+            Class build = Build.class;
+            Field serial = build.getDeclaredField("SERIAL");
+            serial.setAccessible(true);
+            serial.set(null, "0123456789");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String[] hookItemNames = {
+//        "lab.galaxy.changephoneinfo.Hook_GsmCellLocation_getCid",
+//        "lab.galaxy.changephoneinfo.Hook_GsmCellLocation_getLac",
+//        "lab.galaxy.changephoneinfo.Hook_BluetoothAdapter_getAddress",
+//        "lab.galaxy.changephoneinfo.Hook_TelephonyManager_getSimOperator",
+//        "lab.galaxy.changephoneinfo.Hook_TelephonyManager_getLine1Number",
+//        "lab.galaxy.changephoneinfo.Hook_TelephonyManager_getDeviceId",
+//        "lab.galaxy.changephoneinfo.Hook_TelephonyManager_getSubscriberId",
+//        "lab.galaxy.changephoneinfo.Hook_TelephonyManager_getSimSerialNumber",
+//        "lab.galaxy.changephoneinfo.Hook_Location_getLatitude",
+//        "lab.galaxy.changephoneinfo.Hook_Location_getLongitude",
+//        "lab.galaxy.changephoneinfo.Hook_WifiInfo_getBSSID",
+//        "lab.galaxy.changephoneinfo.Hook_WifiInfo_getSSID",
+//        "lab.galaxy.changephoneinfo.Hook_WifiInfo_getIpAddress",
+//        "lab.galaxy.changephoneinfo.Hook_WifiInfo_getMacAddress"
+        "lab.galaxy.changephoneinfo.Hook_String_toString"
+    };
+}
